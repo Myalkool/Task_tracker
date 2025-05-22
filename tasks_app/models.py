7698980314284dc94bea0521bad4386763002649
Myalkool <108334262+Myalkool@users.noreply.github.com>
2025-05-22 13:35:59 +0300

    Update models.py

--- tasks_app/models.py
+++ tasks_app/models.py
@@ -13,6 +13,7 @@ class Task(models.Model):
     telegram_user_id = models.BigIntegerField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='undone')
     created_at = models.DateTimeField(auto_now_add=True)
+    notified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
